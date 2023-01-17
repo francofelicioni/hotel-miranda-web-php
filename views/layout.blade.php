53d8351067f9f704d60ace398f2c105db305a976
Franco Felicioni <fran.felicioni@gmail.com>
2023-01-17 16:51:05 +0100

    Views created *.blade.php for all pages

---
+++ views/layout.blade.php
@@ -0,0 +1,156 @@
+<header class="header" id="header">
+      <div class="header__container">
+        <div class="nav-bar">
+          <nav class="nav-bar__container">
+            <ul class="nav-bar__ul">
+              <li><a href="./pages/about.html">About Us</a></li>
+              <li><a href="./pages/rooms.html">Rooms</a></li>
+              <li><a href="./pages/offers.html">Offers</a></li>
+              <li><a href="./pages/contact.html">Contact</a></li>
+            </ul>
+          </nav>
+          <label for="menu" class="nav-bar__burger-menu">
+            <span id="span1"></span>
+            <span id="span2"></span>
+            <span id="span3"></span>
+          </label>
+        </div>
+        <div class="logo">
+          <div class="logo-sm__container">
+            <img
+              class="logo-sm"
+              src="./assets/icons/header/h.svg"
+              alt="Short Logo"
+            />
+          </div>
+          <img
+            class="logo__lg"
+            src="./assets/icons/header/hotelMiranda.svg"
+            alt="Full Logo"
+          />
+        </div>
+      </div>
+      <div class="header__container">
+        <div class="icon-container">
+          <img
+            class="icon"
+            src="./assets/icons/header/account.svg"
+            alt="Profile Icon"
+          />
+        </div>
+        <div class="icon-container">
+          <img
+            class="icon"
+            src="./assets/icons/header/search.svg"
+            alt="Search Icon"
+          />
+        </div>
+      </div>
+    </header>
+
+    <footer class="footer">
+      <div class="footer__container">
+        <div class="footer__information">
+          <div class="footer__information__logo">
+            <div class="sm-logo-container">
+              <p class="sm-logo">H</p>
+            </div>
+            <img
+              class="lg-logo"
+              src="./assets/icons/header/hotelMiranda.svg"
+              alt="Full Logo"
+            />
+          </div>
+          <p class="footer__information__description">
+            Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed doing
+            eius mod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitat ion ullamco laboris nisi.
+          </p>
+          <div class="social-icons__container">
+            <a class="social-icon__item">
+              <img src="./assets/icons/footer/fb.svg" alt="Facebook logo" />
+            </a>
+            <a class="social-icon__item">
+              <img src="./assets/icons/footer/be.svg" alt="Twitter logo" />
+            </a>
+            <a class="social-icon__item">
+              <img
+                class="twitter-icon"
+                src="./assets/icons/footer/tw.svg"
+                alt="Bahance logo"
+              />
+            </a>
+            <a class="social-icon__item">
+              <img src="./assets/icons/footer/in.svg" alt="Linkedin logo" />
+            </a>
+            <a class="social-icon__item">
+              <img src="./assets/icons/footer/yt.svg" alt="Youtube logo" />
+            </a>
+          </div>
+        </div>
+        <div class="footer__services">
+          <h6>Services.</h6>
+          <div class="footer__services__container">
+            <div class="footer__services__columun1">
+              <p class="services_item">+ Bar</p>
+              <p class="services_item">+ Restaurant</p>
+              <p class="services_item">+ Swimming Pool</p>
+              <p class="services_item">+ Wellness & Spa</p>
+              <p class="services_item">+ Conference Room</p>
+              <p class="services_item">+ Cocktail Party House</p>
+            </div>
+            <div class="footer__services__columun2">
+              <p class="services_item">+ Gaming Zone</p>
+              <p class="services_item">+ Marriage Party</p>
+              <p class="services_item">+ Party Planning</p>
+              <p class="services_item">+ Tour Consultancy</p>
+            </div>
+          </div>
+        </div>
+        <div class="footer__contact-us">
+          <h6>Contact Us.</h6>
+          <div class="contact-us__container">
+            <img src="./assets/icons/footer/phone.svg" alt="Phone logo" />
+            <div class="contact-us_information-container">
+              <p class="information__container__item">Phone Number</p>
+              <a
+                href="tel:987 876 765 76 577"
+                class="information__container__item-sm"
+                >+987 876 765 76 577</a
+              >
+            </div>
+          </div>
+          <div class="contact-us__container">
+            <img src="./assets/icons/footer/mail.svg" alt="Mail logo" />
+            <div class="contact-us_information-container">
+              <p class="information__container__item">E-mail</p>
+              <a
+                href="mailto:contact@hotelmiranda.com"
+                class="information__container__item-sm"
+              >
+                contact@hotelmiranda.com
+              </a>
+            </div>
+          </div>
+          <div class="contact-us__container">
+            <img src="./assets/icons/footer/phone.svg" alt="Phone logo" />
+            <div class="contact-us_information-container">
+              <p class="information__container__item">Our Address</p>
+              <p class="information__container__item-sm">
+                123 Main St, New York
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer__copyright-policies">
+        <div class="footer__copyright">
+          <p>Copyright By@Example - 2022</p>
+        </div>
+        <div class="footer__policies">
+          <a href="#">Terms of use</a>
+          <p>|</p>
+          <a href="#">Privacy Environmental Policy</a>
+        </div>
+      </div>
+    </footer>
