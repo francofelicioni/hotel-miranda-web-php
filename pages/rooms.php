902fd97f9dfd5b9245159930684fd85822dd74f7
Franco Felicioni <fran.felicioni@gmail.com>
2023-01-17 16:50:18 +0100

    Pages created in .php
    With logic of BladeOne

---
+++ pages/rooms.php
@@ -0,0 +1,8 @@
+<?php
+include "../blade/lib/BladeOne.php";
+use eftec\bladeone;
+
+$views = '../views';
+$compiledFolder = '../compiled';
+$blade = new bladeone\BladeOne($views, $compiledFolder);
+echo $blade->run("rooms");
